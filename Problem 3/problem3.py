adec6c0ed82b85c739fc4662d8f4920e2611210e
Roshan Shrestha <roshanpra@gmail.com>
2018-01-30 22:10:50 +0545

    Update problem3.py

--- Problem 3/problem3.py
+++ Problem 3/problem3.py
@@ -1,10 +1,10 @@
-num = 600851475143  
-i=2
-while i<num:
-	if num%i==0:
-		num=num/i
-		
-		
-	    
-	i=i+1
-print(num)
+def lpf(x):
+	lpf=2
+	while (x>lpf):
+		if (x%lpf==0):
+			x=x/lpf
+			
+		else:
+			lpf+=1
+	return x
+print(lpf(600851475143))
